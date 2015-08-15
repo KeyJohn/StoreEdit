ec6c6fb8d71ea04be7bedcde0545b766d3a0be02
keyzhang <843387173@qq.com>
2015-08-15 11:54:16 +0800

    8.15

---
+++ StoreEdit/MyBrige.h
@@ -0,0 +1,14 @@
+//
+//  MyBrige.h
+//  StoreEdit
+//
+//  Created by keyzhang on 15/8/15.
+//  Copyright © 2015年 keyzhang. All rights reserved.
+//
+
+#ifndef MyBrige_h
+#define MyBrige_h
+
+#import "EGODatabase.h"
+
+#endif /* MyBrige_h */
